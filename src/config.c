96ed23d3c58e9897ae54f5dae195cbae2d05ea52
Valeriy Kireev <valeriykireev@gmail.com>
2017-07-02 20:26:31 +0300

    Fix code style

--- src/config.c
+++ src/config.c
@@ -2,23 +2,14 @@
 #include "config.h"
 #include "itmmorgue.h"
 
-enum config_parser_retval {
-    CP_SUCCESS,  // Option was parsed successfully
-    CP_NO_VALUE, // Given string doesn't contain "="
-    CP_NO_KEY,   // "=" is the first character
-    CP_TOO_LONG, // Option is longer than it can be
-    CP_UNDEF     // An error occurred, but behaviour is undefined
-};
-
-static enum config_parser_retval
-parse_option(const char *opt, conf_t *rc, enum conf_type ct, char *key);
+static enum config_parser_retval parse_option(const char *opt, conf_t *rc,
+        enum conf_type ct, char *key);
 
 // Parsers for concrete value types, called by parse_option()
-static enum config_parser_retval
-parse_option_string(const char *opt, conf_t *rc);
+static enum config_parser_retval parse_option_string(const char *opt,
+        conf_t *rc);
 
-static enum config_parser_retval
-parse_option_int(const char *opt, conf_t *rc);
+static enum config_parser_retval parse_option_int(const char *opt, conf_t *rc);
 
 static enum config_parser_retval (*parsers[])(const char *opt, conf_t *rc) = {
     parse_option_string,
@@ -114,8 +105,8 @@ conf_t conf(char *key) {
  *
  * ret : one of config_parser_retval enumeration values
  */
-static enum config_parser_retval
-parse_option(const char *opt, conf_t *rc, enum conf_type ct, char *key) {
+static enum config_parser_retval parse_option(const char *opt, conf_t *rc,
+        enum conf_type ct, char *key) {
     if (NULL == opt) return CP_SUCCESS;
     if (NULL == opt) {
         panic("NULL passed as a config option");
@@ -148,12 +139,6 @@ parse_option(const char *opt, conf_t *rc, enum conf_type ct, char *key) {
         return CP_UNDEF;
     }
 
-    // clangs tells it's always false.
-    // It will be truth until somebody casts int to enum.
-    //if (ct > sizeof(parsers) / sizeof(parsers[0])) {
-    //    panic("Trying to call undefined parser!");
-    //}
-
     // Save key
     // TODO: test it all!
     if (NULL != key) {
@@ -172,8 +157,8 @@ parse_option(const char *opt, conf_t *rc, enum conf_type ct, char *key) {
  *
  * ret : one of config_parser_retval enumeration values
  */
-static enum config_parser_retval
-parse_option_string(const char *opt, conf_t *rc) {
+static enum config_parser_retval parse_option_string(const char *opt,
+        conf_t *rc) {
     rc->sval = (char*) malloc(strnlen(opt, MAX_OPT_LEN));
     if (NULL == rc->sval) {
         panicf("Allocation failed in file %s, line %d", __FILE__, __LINE__);
@@ -189,8 +174,8 @@ parse_option_string(const char *opt, conf_t *rc) {
  *
  * ret : one of config_parser_retval enumeration values
  */
-static enum config_parser_retval
-parse_option_int(const char *opt, conf_t *rc) {
+static enum config_parser_retval parse_option_int(const char *opt,
+        conf_t *rc) {
     char *endptr = NULL;
     int value = strtoi(opt, &endptr, 0 /* Not only decimal */);
 
--- src/config.h
+++ src/config.h
@@ -4,6 +4,14 @@
 
 #include <string.h>
 
+enum config_parser_retval {
+    CP_SUCCESS,  // Option was parsed successfully
+    CP_NO_VALUE, // Given string doesn't contain "="
+    CP_NO_KEY,   // "=" is the first character
+    CP_TOO_LONG, // Option is longer than it can be
+    CP_UNDEF     // An error occurred, but behaviour is undefined
+};
+
 // Type of configuration entity
 enum conf_type {
     CONF_STRING = 0,
@@ -16,7 +24,7 @@ typedef struct conf {
         int ival;
         char *sval;
     } value;             // config value
-#define ival value.ival  // ugly definitons for quick access to values
+#define ival value.ival  // ugly definitions for quick access to values
 #define sval value.sval
     enum conf_type type; // type of value
 } conf_t;
