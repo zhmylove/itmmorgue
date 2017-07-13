193aee747dc1b1caf9a72383eeb3a635c22ec1a4
zhmylove <zhmylove@narod.ru>
2017-07-13 18:06:28 +0300

    Implement protocol version generation

--- src/area.c
+++ src/area.c
@@ -56,6 +56,8 @@ void draw_area() {
     mvwprintw(W(W_AREA), 5, 1, "%s %d", _("Connection:"), server_connected);
     mvwprintw(W(W_AREA), 6, 1, "%s %s", _("Server address:"), server_address);
     mvwprintw(W(W_AREA), 7, 1, "%s %s", _("Server address:"), nickname);
+    mvwprintw(W(W_AREA), 8, 1, "%s %d", _("Protocol:"), PROTOCOL_VERSION);
+
     return;
 }
 
