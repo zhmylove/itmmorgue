3c11e47d3ff1b51d9a2f2d16e45d9ebd2da0a9df
zhmylove <zhmylove@narod.ru>
2017-12-20 01:42:22 +0300

    Use map for hardcoded building

--- scripts/gen_castle.pl
+++ scripts/gen_castle.pl
@@ -14,25 +14,25 @@ use gen;
 # Read a level into memory from STDIN
 gen->read_level();
 
-my @CASTLE = (
-   [ split //, "###########" ],
-   [ split //, "#_________#" ],
-   [ split //, "#__#___#__#" ],
-   [ split //, "#__#___#__#" ],
-   [ split //, "#__#___#__#" ],
-   [ split //, "#__#_C_#__#" ],
-   [ split //, "#__#___#__#" ],
-   [ split //, "#__#####__#" ],
-   [ split //, "#_________#" ],
-   [ split //, "#####_#####" ],
-   [ split //, "    #_#    " ],
-   [ split //, "^ ^ #_# ^ ^" ],
-   [ split //, " ^  #_#  ^ " ],
-   [ split //, "^ ^     ^ ^" ],
+my @CASTLE = map { [ split // ] } (
+   "###########" ,
+   "#_________#" ,
+   "#__#___#__#" ,
+   "#__#___#__#" ,
+   "#__#___#__#" ,
+   "#__#_C_#__#" ,
+   "#__#___#__#" ,
+   "#__#####__#" ,
+   "#_________#" ,
+   "#####_#####" ,
+   "    #_#    " ,
+   "^ ^ #_# ^ ^" ,
+   " ^  #_#  ^ " ,
+   "^ ^     ^ ^" ,
 );
 
 # Put some buildings on the level
-gen->overlay_anywhere(\@CASTLE, 2, 'x');
+gen->overlay_anywhere(\@CASTLE, 2, ',');
 gen->overlay_anywhere(\@CASTLE);
 
 # Print the surface
