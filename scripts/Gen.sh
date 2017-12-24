1bc5e6a2d287de121b506e01ff693499ba7e1905
Sergey Zhmylove <zhmylove@narod.ru>
2017-12-25 00:59:07 +0300

    Add client messages to Gen.sh

--- scripts/Gen.sh
+++ scripts/Gen.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 # Made by: KorG
 
-perl gen_surface.pl -w256 -h64                        |
-perl gen_fields.pl                                    |
-perl gen_fields.pl -f                                 |
-perl gen_castle.pl                                    |
-perl gen_placeholders.pl                              |
+pusk() {
+   echo "Running: $1" >&2
+   shift
+   perl "$@"
+}
+
+pusk "surface creation"       gen_surface.pl -w256 -h64                |
+pusk "fields normalization"   gen_fields.pl                            |
+pusk "fields correction"      gen_fields.pl -f                         |
+pusk "castles creation"       gen_castle.pl                            |
+pusk "final preparations"     gen_placeholders.pl                      |
 less -S
