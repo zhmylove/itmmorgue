e91b5cf652f43ea1a0ec8396ac03d77fff773aae
zhmylove <zhmylove@narod.ru>
2017-12-28 16:59:12 +0300

    Improve cities generation

--- src/default_config.h
+++ src/default_config.h
@@ -16,8 +16,8 @@ struct t_conf_default {
     char *key;
     conf_t value;
 } t_conf_default[] = {
-    C_INT("level_width", 256),
-    C_INT("level_height", 64),
+    C_INT("level_width", 512),
+    C_INT("level_height", 256),
 
     C_INT("win_stdscr_small_y", 0),
     C_INT("win_stdscr_small_y_ispercent", 0),
