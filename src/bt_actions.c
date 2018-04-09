a5f6e5a0f94d5f92c69551e4f79b7d9e9addb51a
Yaroslav Rogov <rogovyaroslav@gmail.com>
2018-04-09 20:22:50 +0300

    Add bt_actions: common actions shared by multiple BTs

---
+++ src/bt_actions.c
@@ -0,0 +1,77 @@
+// vim: et sw=4 ts=4 :
+
+#include "bt_actions.h"
+
+#include "entity.h"
+
+enum bt_node_status square_move(entity_t* e, void* context){
+    struct square_move_context* ctx = (struct square_move_context*) context;
+
+    #define X e->x
+    #define Y e->y
+    #define UL_X ctx->ul_corner.x
+    #define UL_Y ctx->ul_corner.y
+    #define SQ_LEN ctx->side_length
+    #define DIR ctx->step_direction
+
+    switch(ctx->direction){
+        case ROT_CW:
+            do{
+                switch(DIR){
+                    case DIR_E:
+                        if( X >= UL_X + SQ_LEN ){ DIR = DIR_S; continue; }
+                        X++;
+                        break;
+
+                    case DIR_S:
+                        if( Y >= UL_Y + SQ_LEN){ DIR = DIR_E; continue; }
+                        Y++;
+                        break;
+
+                    case DIR_W:
+                        if( X <= UL_X ){ DIR = DIR_N; continue; }
+                        X--;
+                        break;
+
+                    case DIR_N:
+                        if( Y <= UL_Y ){ DIR = DIR_W; continue; }
+                        Y--;
+                        break;
+                    }
+            } while(0);
+
+        case ROT_CCW:
+            do{
+                switch(DIR){
+                    case DIR_S:
+                        if( Y >= UL_Y + SQ_LEN ){ DIR = DIR_E; continue; }
+                        Y++;
+                        break;
+
+                    case DIR_E:
+                        if( X >= UL_X + SQ_LEN){ DIR = DIR_N; continue; }
+                        X++;
+                        break;
+
+                    case DIR_N:
+                        if( Y <= UL_Y ){ DIR = DIR_E; continue; }
+                        Y--;
+                        break;
+
+                    case DIR_W:
+                        if( X <= UL_X ){ DIR = DIR_S; continue; }
+                        X--;
+                        break;
+                }
+            } while(0);
+    }
+
+    #undef X
+    #undef Y
+    #undef UL_X
+    #undef UL_Y
+    #undef SQ_LEN
+    #undef DIR
+
+    return BT_RUNNING;
+}
---
+++ src/bt_leaf.h
@@ -0,0 +1,20 @@
+#ifndef _BT_LEAF_H_
+#define _BT_LEAF_H_
+
+enum rotation_direction {
+    ROT_CW = 0,
+    ROT_CCW
+};
+
+enum direction {
+    DIR_N=0,
+    DIR_NE,
+    DIR_E,
+    DIR_SE,
+    DIR_S,
+    DIR_SW,
+    DIR_W,
+    DIR_NW,
+};
+
+#endif /* _BT_LEAF_H_ */
